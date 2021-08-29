7d8978f1f57a0e17f82f996b46b0b2a42e857f8f
Jason Dsouza <jasondsouza212@gmail.com>
2021-08-29 19:24:16 +0530

    edited the finding in infinite array error with arr.length

--- searchingininfinitearray.java
+++ searchingininfinitearray.java
@@ -1,18 +1,24 @@
 public class searchingininfinitearray {
     public static void main(String[] args) {
-        int[] arr={1,2,4,5,6,29,45,50};
-       int ans= position(arr,29);
+        int[] arr={3, 5, 7, 9, 10, 90,
+                100, 130, 140, 160, 170};
+       int ans= position(arr,170);
         System.out.println(ans);
 
     }
     static int position(int arr[],int target){
         int start=0;
         int end=1;
-        int count=2;
+
         while(target>arr[end]){
-            int temp=end+1;
-            end=end+(end-start+1)*2;
-            start=temp;
+                int temp=end+1;
+                end=end+(end-start+1)*2;
+                if(end>arr.length-1){
+                    end= arr.length-1;
+                    break;
+                }
+                start=temp;
+
         }
         int ans=search(arr,target,start,end);
         return  ans;
