45794fac634bdcec212297ac742e40c7713bb6b2
Jason Dsouza <jasondsouza212@gmail.com>
2021-09-01 12:34:50 +0530

    code changed for the searching in cycle array with dublictes elements

--- countingthenoofcycles.java
+++ countingthenoofcycles.java
@@ -1,27 +1,38 @@
 public class countingthenoofcycles {
     public static void main(String[] args) {
-        int[] arr={3,4,5,1,2};
+        int[] arr = {3, 4, 5, 1, 2};
         System.out.println(ans(arr));
     }
-    static  int ans(int arr[]){
-        int ans=cycles(arr);
-        return  ans+1;
+
+    static int ans(int arr[]) {
+        int ans = cycles(arr);
+        return ans + 1;
     }
-    static int cycles(int arr[]){
-        int start=0;
-        int end=arr.length-1;
-        while(start<=end){
-            int mid=start+(end-start)/2;
-            if(mid<end && arr[mid]>arr[mid+1]){
+
+    static int cycles(int arr[]) {
+        int start = 0;
+        int end = arr.length - 1;
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+            if (mid < end && arr[mid] > arr[mid + 1]) {
                 return mid;
             }
-            if(mid>start && arr[mid]<arr[mid-1]){
-                return mid-1;
+            if (mid > start && arr[mid] < arr[mid - 1]) {
+                return mid - 1;
             }
-            if(arr[mid]<=arr[start]){
-                end=mid-1;
-            }else{
-                start=mid+1;
+            if (arr[mid] == arr[start] && arr[mid] == arr[end]) {
+                if (arr[start] > arr[start + 1]) {
+                    return start;
+                }
+                start++;
+                if (arr[end] < arr[end] - 1) {
+                    return end - 1;
+                }
+                end--;
+            } else if (arr[start] < arr[mid] || (arr[start] == arr[mid] && arr[mid] > arr[end])) {
+                start = mid + 1;
+            } else {
+                end = mid - 1;
             }
         }
         return -1;
